f9887e8f6d6bbcb2a3c54ef673cbda89578bf227
Yagya Adh <yagyaadhikari02@gmail.com>
2024-12-04 12:50:46 +0545

    footer save the changes with copyright section

--- app/components/section/CopyRightFooter.tsx
+++ app/components/section/CopyRightFooter.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const CopyRightFooter = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <section className="max-w-screen-3xl mx-auto w-full flex flex-col md:flex-row items-center justify-center md:justify-between border-t-2 border-t-black">
-      <p className="text-2xl py-10 font-inter font-light">
+    <section className="max-w-screen-3xl mx-auto w-full flex flex-col md:flex-row items-center justify-center md:justify-between border-t-2 border-t-black py-10">
+      <p className="md:text-2xl py-4 font-inter font-normal md:font-light text-center max-w-60 md:max-w-xl md:px-2">
         © {currentYear} Cleaners. All Rights Reserved.
       </p>
-      <p className="text-2xl py-10 font-inter font-light">
+      <p className="md:text-2xl py-4 font-inter font-normal md:font-light text-center max-w-52 md:max-w-xl md:px-2">
         Powered By{" "}
         <a href="https://wwww.jamstackers.com" target="_blank">
           Jamstackers
