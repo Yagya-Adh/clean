c2d27df6210fa7f12fcd741aeeed6eba6f740d45
Yagya Adh <yagyaadhikari02@gmail.com>
2024-12-09 18:14:21 +0545

    update hero section banner text size

--- app/components/banner/MainHeroBanner.tsx
+++ app/components/banner/MainHeroBanner.tsx
@@ -3,19 +3,19 @@ import { SprayIcon } from "@/app/icons";
 const MainHeroBanner = () => {
   return (
     <section className="max-w-screen-4xl mx-auto px-4 md:px-10 lg:px-20 py-4">
-      <div className="rounded-4xl bg-hero-main bg-no-repeat bg-cover p-10 sm:px-24 md:px-4 lg:py-80 max-w-full flex flex-col justify-center">
-        <div className="relative lg:-top-72 left-0 flex flex-col justify-center items-center text-center font-inter z-10 border border-white/15 backdrop:blur-3xl rounded-3xl p-10 md:p-4 md:w-1/2 mx-auto bg-white/15 backdrop-blur-2xl">
+      <div className="rounded-4xl bg-hero-main bg-no-repeat bg-cover p-6  lg:py-80 max-w-full flex flex-col justify-center">
+        <div className="relative lg:-top-64 left-0 flex flex-col justify-center items-center text-center font-inter z-10 border border-white/15 backdrop:blur-3xl rounded-3xl p-10 md:p-4 md:w-1/2 mx-auto bg-white/15 backdrop-blur-2xl">
           <div className="flex flex-row justify-center items-center mb-4 w-full">
             <SprayIcon className="w-8 h-10" />
-            <h3 className="text-base md:text-xl text-clean-black-10 ml-2 uppercase font-inter font-normal">
+            <h3 className="text-sm lg:text-xl text-clean-black-10 ml-2 uppercase font-inter font-normal">
               Go-to Cleaners
             </h3>
           </div>
-          <h1 className="font-inter font-light text-3xl sm:text-4xl md:text-7xl xl:text-8xl py-6 uppercase animate-flipTopDownIn">
+          <h1 className="font-inter font-light text-4xl sm:text-5xl lg:text-7xl xl:text-8xl sm:py-4 uppercase animate-flipTopDownIn">
             Expert <br />
             <span className="font-thin italic">Cleaning</span>
           </h1>
-          <p className="text-center text-base md:text-2xl text-clean-black-10 font-inter font-light max-w-md md:max-w-xl animate-flipTopDownIn">
+          <p className="text-center text-base   text-clean-black-10 font-inter font-light max-w-md md:max-w-xl animate-flipTopDownIn">
             Creating cleaner, healthier spaces so you can focus on what matters
             most and relax on the weekends.
           </p>
