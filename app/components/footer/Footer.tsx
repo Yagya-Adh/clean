bd5cd308e0e189941e3baecc939ed6d6b0c7f635
Yagya Adh <yagyaadhikari02@gmail.com>
2024-12-11 10:56:35 +0545

    footer alignment and text-resize

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <RequestAFreeQuote />
       </div>
       <section className="max-w-screen-5xl px-4 lg:px-10 mx-auto relative py-4 2xl:py-20 -translate-y-24">
-        <div className="flex flex-col justify-center sm:flex-row lg:grid lg:grid-cols-2 items-center md:grid-cols-2 gap-0 sm:gap-10  2xl:gap-80 place-items-start h-full w-full 2xl:w-full">
+        <div className="flex flex-col justify-center sm:flex-row sm:justify-end sm:items-start lg:flex-row lg:justify-between items-center md:grid-cols-2 gap-0 sm:gap-10 2xl:flex 2xl:justify-between 2xl:px-20 2xl:gap-80 place-items-start h-full w-full 2xl:w-full">
           <div>
             <FooterLeft />
           </div>
--- app/components/footer/FooterRight.tsx
+++ app/components/footer/FooterRight.tsx
@@ -14,22 +14,22 @@ interface IfooterRightData {
 const data: IfooterRightData[] = footerRightData;
 const FooterRight = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4 xl:gap-10 2xl:gap-56 text-center md:text-start md:place-items-start lg:py-0 py-4">
+    <section className="grid grid-cols-1 md:grid-cols-3 gap-6 2xl:gap-20 md:gap-4 text-center md:text-start md:place-items-start lg:py-0 py-4">
       {data?.slice(0, 3).map((list) => (
         <div
-          className="flex flex-col justify-center w-full items-center py-2 2xl:py-0"
+          className="flex flex-col justify-end w-full items-center 2xl:py-0"
           key={list.id}
         >
-          <h3 className="text-clean-black-10 text-lg lg:text-lg  2xl:text-2xl text-text-center font-medium sm:font-normal font-inter tracking-widest">
+          <h3 className="text-clean-black-10 text-xs 2xl:text-xl text-text-center font-medium sm:font-normal font-inter tracking-widest">
             {list.title}
           </h3>
           {list.innerItem?.map((inner) => (
             <Link href={inner.linkPath} key={inner.id}>
               <div className="text-center overflow-hidden h-10 sm:h-12 group md:my-2">
-                <div className="tracking-widest translate-y-4 group-hover:-translate-y-16 ease-in-out transition-all duration-500 text-clean-black-10 text-base sm:text-xs lg:text-xl 2xl:text-2xl 2xl:font-normal 2xl:max-w-xl 2xl:tracking-wide font-normal font-inter h-10">
+                <div className="tracking-wide 2xl:tracking-normal translate-y-4 group-hover:-translate-y-16 ease-in-out transition-all duration-500 text-clean-black-10 text-xs xl:text-2xl font-normal font-inter h-10">
                   {inner.item}
                 </div>
-                <div className="tracking-widest translate-y-9 group-hover:-translate-y-6 ease-in-out transition-all duration-500 text-clean-black-10 text-base sm:text-xs lg:text-xl 2xl:text-2xl 2xl:font-normal 2xl:max-w-xl 2xl:tracking-wide font-normal font-inter h-10">
+                <div className="tracking-wide 2xl:tracking-normal translate-y-9 group-hover:-translate-y-6 ease-in-out transition-all duration-500 text-clean-black-10 text-xs xl:text-xl 2xl:font-normal 2xl:max-w-xl font-normal font-inter h-10">
                   {inner.item}
                 </div>
               </div>
